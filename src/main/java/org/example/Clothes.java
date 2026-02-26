335b22e96ca21a10ccf4e3b1e7bb9dd67afdaf97
Pavlo Mesha <mesha.pavlo@student.sumdu.edu.ua>
2026-02-26 23:33:40 +0200

    Implement toString and equals for Clothes class

--- src/main/java/org/example/Clothes.java
+++ src/main/java/org/example/Clothes.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Clothes {
     private String type;
     private String size;
     private double price;
 
+    // Конструктор
     public Clothes(String type, String size, double price) {
         this.type = type;
         this.size = size;
@@ -18,4 +21,19 @@ public class Clothes {
     public void setSize(String size) { this.size = size; }
     public double getPrice() { return price; }
     public void setPrice(double price) { this.price = price; }
+
+    // Метод toString для виводу інформації
+    @Override
+    public String toString() {
+        return "Одяг: " + type + ", Розмір: " + size + ", Ціна: " + price + " грн";
+    }
+
+    // Метод equals для порівняння об'єктів
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clothes clothes = (Clothes) o;
+        return Double.compare(clothes.price, price) == 0 && Objects.equals(type, clothes.type) && Objects.equals(size, clothes.size);
+    }
 }
