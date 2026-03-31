c2dcb1904bcaa24399357caa1b2b624ee90d2a61
meshapavlo-glitch <mesha.pavlo@student.sumdu.edu.ua>
2026-03-31 21:27:48 +0300

    Validate pants length in constructor
    Added validation for pants length in constructor.

--- src/main/java/org/example/Pants.java
+++ src/main/java/org/example/Pants.java
@@ -5,13 +5,15 @@ public class Pants extends Clothes {
 
     public Pants(String type, String size, double price, Material material, int length) {
         super(type, size, price, material);
+        if (length < 20 || length > 150) {
+            throw new IllegalArgumentException("Довжина штанів має бути в межах 20-150 см");
+        }
         this.length = length;
     }
 
-      public int getLength() {
+    public int getLength() {
         return length;
     }
-
 
     @Override
     public String toString() {
