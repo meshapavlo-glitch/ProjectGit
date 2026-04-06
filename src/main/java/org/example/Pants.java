d78309f5747235225821eb9fda5324214acd81a7
meshapavlo-glitch <mesha.pavlo@student.sumdu.edu.ua>
2026-04-06 21:13:41 +0300

    Add exception handling to Pants constructor
    Updated the Pants constructor to include exception handling for invalid length values.

--- src/main/java/org/example/Pants.java
+++ src/main/java/org/example/Pants.java
@@ -3,7 +3,9 @@ package org.example;
 public class Pants extends Clothes {
     private int length; // Довжина штанин
 
-    public Pants(String type, String size, double price, Material material, int length) {
+    public Pants(String type, String size, double price, Material material, int length)
+            throws InvalidFieldValueException
+    {
         super(type, size, price, material);
         if (length < 20 || length > 150) {
             throw new IllegalArgumentException("Довжина штанів має бути в межах 20-150 см");
